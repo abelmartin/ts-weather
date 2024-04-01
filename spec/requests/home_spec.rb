ed8a2440d29f975f9bbe106a9380ae84806e196d
abelmartin <abel.martin@gmail.com>
2024-03-31 21:19:13 -0400

    Added a little styling on the project along with a request spec that needs to be completed.

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
+  before do
+    allow(Forecaster).to receive(:call)
+  end
+
   describe "GET /index" do
-    it "returns http success" do
-      get "/home/index"
+    it "returns page with expectd markup" do
+      get "/"
       expect(response).to have_http_status(:success)
+      expect(response.body).to include('TS Weather Forcaster')
+    end
+
+    it "returns empty json object for json call" do
+      get "/", as: :json
+      expect(response).to have_http_status(:success)
+      expect(response.body).to eq('{}')
     end
   end
 
